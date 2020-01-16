7fd1f445f25145c06d22757184b066e78b640378
AndrewIDFK <47414739+AndrewIDFK@users.noreply.github.com>
2020-01-16 20:16:13 +0200

    Add files via upload

---
+++ Items/AmpedAmethyst.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Terraria;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Archeon.Items
+{
+	public class AmpedAmethyst : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			base.DisplayName.SetDefault("Amplified Amethyst");
+			base.Tooltip.SetDefault("An amethyst amplified by a Nightly Soul");
+		}
+
+		public override void SetDefaults()
+		{
+			base.item.width = 12;
+			base.item.height = 12;
+			base.item.rare = 6;
+			base.item.value = Item.buyPrice(0, 0, 35, 0);
+			base.item.value = Item.sellPrice(0, 0, 35, 0);
+			base.item.maxStack = 99;
+		}
+		
+		public override void AddRecipes()
+		{
+			ModRecipe modRecipe = new ModRecipe(base.mod);
+			modRecipe.AddIngredient(181, 1);
+			modRecipe.AddIngredient(521, 1);
+			modRecipe.AddTile(mod.TileType("GemConstructorTile"));
+			modRecipe.SetResult(this, 1);
+			modRecipe.AddRecipe();
+		}
+	}
+}
---
+++ Items/AmpedRuby.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Terraria;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Archeon.Items
+{
+	public class AmpedRuby : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			base.DisplayName.SetDefault("Amplified Ruby");
+			base.Tooltip.SetDefault("A ruby amplified by a durable material");
+		}
+
+		public override void SetDefaults()
+		{
+			base.item.width = 12;
+			base.item.height = 12;
+			base.item.rare = 6;
+			base.item.value = Item.buyPrice(0, 0, 60, 0);
+			base.item.value = Item.sellPrice(0, 0, 60, 0);
+			base.item.maxStack = 99;
+		}
+		
+		public override void AddRecipes()
+		{
+			ModRecipe modRecipe = new ModRecipe(base.mod);
+			modRecipe.AddIngredient(178, 1);
+			modRecipe.AddIngredient(366, 1);
+			modRecipe.AddTile(mod.TileType("GemConstructorTile"));
+			modRecipe.SetResult(this, 1);
+			modRecipe.AddRecipe();
+			
+			modRecipe = new ModRecipe(base.mod);
+			modRecipe.AddIngredient(178, 1);
+			modRecipe.AddIngredient(1106, 1);
+			modRecipe.AddTile(mod.TileType("GemConstructorTile"));
+			modRecipe.SetResult(this, 1);
+			modRecipe.AddRecipe();
+		}
+	}
+}
