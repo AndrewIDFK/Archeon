964e6f462d4a38c09438d7b1aa16260ba4f99101
AndrewIDFK <47414739+AndrewIDFK@users.noreply.github.com>
2021-07-29 19:34:52 +0300

    Delete Properties directory

--- Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: CompilationRelaxations(8)]
-[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
